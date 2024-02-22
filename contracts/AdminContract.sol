8cdb9d284cb5ff4b5671841015b22b2a9da16504
Denis Kartashov <kartashov.denis1993@gmail.com>
2024-02-22 16:50:26 +0700

    AdminContract and AccessControl on factory

---
+++ contracts/AdminContract.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
+
+contract AdminContract is AccessControlEnumerableUpgradeable{
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");
+
+    constructor() {}
+
+    function initialize() public initializer {
+        __AccessControlEnumerable_init();
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(ADMIN_ROLE, _msgSender());
+        _setRoleAdmin(CREATOR_ROLE, ADMIN_ROLE);
+    }
+}
--- contracts/marketplace/collections/Collection-factory.sol
+++ contracts/marketplace/collections/Collection-factory.sol
@@ -2,9 +2,35 @@
 pragma solidity ^0.8.7;
 
 import "./Collection.sol";
+import "../../interfaces/IAdminContract.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract CollectionFactory {
+  bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+  bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");
+  address public adminContract;
+
   event CollectionCreated(address indexed collection, string slug);
+  event AdminContractChanged(address newContract);
+
+  modifier onlyRole(bytes32 role) {
+    require(
+      IAdminContract(adminContract).hasRole(role, msg.sender),
+      string(
+        abi.encodePacked(
+          "AccessControl: account ",
+          Strings.toHexString(uint160(msg.sender), 20),
+          " is missing role ",
+          Strings.toHexString(uint256(role), 32)
+        )
+      )
+    );
+    _;
+  }
+
+  constructor(address _adminContract) {
+    adminContract = _adminContract;
+  }
 
   // CREATOR FUNCTIONS
 
@@ -14,8 +40,14 @@ contract CollectionFactory {
     string calldata slug_,
     address signer_,
     address _gallery
-  ) external returns (address collection) {
+  ) external onlyRole(CREATOR_ROLE) returns (address collection) {
     collection = address(new Collection(msg.sender, name, symbol, slug_, signer_, _gallery));
     emit CollectionCreated(collection, slug_);
+  }
+
+  function setAdminContract(address _adminContract) external onlyRole(ADMIN_ROLE) {
+    require(_adminContract != address(0), "Zero address!");
+    adminContract = _adminContract;
+    emit AdminContractChanged(adminContract);
   }
 }
