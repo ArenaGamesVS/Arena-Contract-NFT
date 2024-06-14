db034b435cc3473128afe72124f41d23c915cbf0
Denis Kartashov <kartashov.denis1993@gmail.com>
2024-06-14 16:49:23 +0700

    add mintBatch and service in constructor

--- contracts/BaseERC20.sol
+++ contracts/BaseERC20.sol
@@ -9,13 +9,22 @@ contract BaseERC20 is ERC20, AccessControl {
   bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-  constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+  constructor(string memory name, string memory symbol, address service) ERC20(name, symbol) {
     _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
     _grantRole(ADMIN_ROLE, _msgSender());
+    _grantRole(ADMIN_ROLE, service);
     _grantRole(MINTER_ROLE, _msgSender());
+    _grantRole(MINTER_ROLE, service);
   }
 
   function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
     _mint(to, amount);
+  }
+
+  function mintBatch(address[] calldata to, uint256[] calldata amount) external onlyRole(MINTER_ROLE) {
+    require(to.length == amount.length, "Array size mismatch");
+    for (uint256 i = 0; i < to.length; i++) {
+      _mint(to[i], amount[i]);
+    }
   }
 }
